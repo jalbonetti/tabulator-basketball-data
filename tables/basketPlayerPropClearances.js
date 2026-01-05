476a1adc75905976e252ba56c2ccb6a1f9b96c5a
jalbonetti <jpalbonetti@gmail.com>
2026-01-04 21:35:24 -0500

    Update basketPlayerPropClearances.js

--- tables/basketPlayerPropClearances.js
+++ tables/basketPlayerPropClearances.js
@@ -482,7 +482,7 @@ export class BasketPlayerPropClearancesTable extends BaseTable {
             {
                 title: "Name", 
                 field: "Player Name", 
-                frozen: true,
+                frozen: isSmallScreen,
                 widthGrow: 0,  // Size to content only - expanded dynamically if needed for subtables
                 minWidth: 120,
                 sorter: "string", 
