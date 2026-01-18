e74bf55e99da60ebc4115d89038c7838c9b59522
jalbonetti <jpalbonetti@gmail.com>
2026-01-17 23:18:26 -0500

    Change longest matchup for width calculation
    Updated the longest matchup string to 'Minnesota Timberwolves @ Oklahoma City Thunder' for accurate width calculation.

--- tables/basketGameOdds.js
+++ tables/basketGameOdds.js
@@ -228,7 +228,7 @@ export class BasketGameOddsTable extends BaseTable {
         
         // UPDATED: Ensure minimum width for Game Matchup accounts for longest possible matchup
         // "Los Angeles Clippers @ Los Angeles Lakers" is the longest possible
-        const longestMatchup = "Los Angeles Clippers @ Los Angeles Lakers";
+        const longestMatchup = "Minnesota Timberwolves @ Oklahoma City Thunder";
         const longestMatchupWidth = ctx.measureText(longestMatchup).width;
         if (longestMatchupWidth > maxWidths["Game Matchup"]) {
             maxWidths["Game Matchup"] = longestMatchupWidth;
